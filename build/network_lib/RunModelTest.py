e918417f638567643992d7412ac6f14cf500247e
Daniel <binaryclock03@gmail.com>
2023-02-23 19:15:18 -0500

    updated filepath in the run and train model files to be relative

--- build/network_lib/RunModelTest.py
+++ build/network_lib/RunModelTest.py
@@ -1,2 +1,2 @@
 import network_main as nm
-nm.run_model_test("saved_models\\trained_model_2023-02-11_13-12-27.h5", "C:\\Users\\binar\\Documents\\Workshop\\School\\ENG4000\\NewNetwork\\data")
+nm.run_model_test("saved_models\\trained_model_2023-02-11_13-12-27.h5", "\\data")
--- build/network_lib/TrainModel.py
+++ build/network_lib/TrainModel.py
@@ -1,2 +1,2 @@
 import network_main as nm
-nm.train_network("C:\\Users\\binar\\Documents\\Workshop\\School\\ENG4000\\NewNetwork\\data")
+nm.train_network("\\data")
